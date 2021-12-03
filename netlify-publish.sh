5b9e798b1939b6485e6c22d3a9c2e4f3f5fe6740
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-12-03 15:27:48 -0600

    ci: Remove not supported netlify command ⚙️

--- netlify-publish.sh
+++ netlify-publish.sh
@@ -2,12 +2,10 @@
 set -e
 
 ## install latest .net until a new netlify image is released...
-pushd /tmp
 wget https://dot.net/v1/dotnet-install.sh -P /tmp
 chmod u+x /tmp/dotnet-install.sh
 /tmp/dotnet-install.sh -c Current
 dotnet new
-popd
 
 dotnet --info
 
